31d8bfeadf0e7281a0b0db07864d5e420d560120
Abhijith Ajith <113827354+AAbhijithA@users.noreply.github.com>
2023-01-25 22:45:56 +0530

    Update Cpp_basic.cpp

--- Cpp_basic.cpp
+++ Cpp_basic.cpp
@@ -2,15 +2,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 void solve(){
-  
+    //Write your code here
 }
 int main(){
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
-  int t;
-  cin >> t;
-  while(t--){
-    solve()
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        solve()
     }
 }
